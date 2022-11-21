93d74691d441f8569cd71a111463956995979b78
guima-seifer <fernando.b.v.guimaraes@gmail.com>
2022-11-21 22:35:50 +0000

    build error fixed

--- components/repos/UserItem.tsx
+++ components/repos/UserItem.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import { User } from '../../model/User'
-import Section from '../layout/section'
+import type { ImageLoaderProps } from 'next/image'
 import Image from 'next/image'
 import TitleSection from '../layout/section/title'
 import Button from '../layout/Button'
 import { FaGithubAlt } from 'react-icons/fa'
+
+
 
 function UserItem({ avatar_url, login }: User) {
   return (
@@ -13,7 +15,7 @@ function UserItem({ avatar_url, login }: User) {
       <div className='mt-8 flex flex-row'>
         <div className='shadow-md'>
           <div className='mx-auto'>
-          <Image className='mx-auto' src={avatar_url} alt='Profile' height={100} width={100} />
+          <Image className='mx-auto' src={avatar_url as string} alt='Profile' height={100} width={100} />
           <h2 className='text-center font-bold'><FaGithubAlt className='inline mr-2' />{login}</h2>
           </div>
           
